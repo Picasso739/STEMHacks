348935e5929c43626b4cd3a2392d3b2dc230a9a6
Ethan Miller <ethan.miller7@icloud.com>
2020-12-24 18:30:05 -0500

    12/24/20 Initial Commit

--- STEMHacks/ViewController.swift
+++ STEMHacks/ViewController.swift
@@ -7,13 +7,48 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    //ui variables
+    @IBOutlet var backgroundView: UIView!
+    @IBOutlet weak var mainStackView: UIStackView!
+    
+    @IBOutlet weak var headerStackView: UIStackView!
+    @IBOutlet weak var menuButton: UIButton!
+    @IBOutlet weak var headerLabel: UILabel!
+    @IBOutlet weak var mailButton: UIButton!
+    
+    @IBOutlet weak var mainSelector: UISegmentedControl!
+    
+    @IBOutlet weak var contentTableView: UITableView!
+    
+    //function to actiavte when the view is loaded
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
+    //function to activate when the menu button is pressed
+    @IBAction func menuButton(_ sender: UIButton) {
+        
+    }
+    
+    //function to activate when the mail button is pressed
+    @IBAction func mailButton(_ sender: UIButton) {
+        
+    }
+    
+    //function to return the number of rows in each section
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    //function to activate to add a view for each cell
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(frame: CGRect(x: 0, y: 0, width: self.view.frame.width, height: 80))
+        
+        return cell
+    }
 
 }
 
